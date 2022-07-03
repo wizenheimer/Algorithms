c34b5ad72bde11558152a60517d164e415fd0df4
nayaN <91504165+wizenheimer@users.noreply.github.com>
2022-07-03 21:56:01 +0530

    Time: 27 ms (22.81%), Space: 15.6 MB (75.31%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    vector<int> solve(vector<int>& input, int target)
+    {
+        int left = 0;
+        int right = input.size()-1;
+        while(left <= right)
+        {
+            int sum = input[left] + input[right];
+            if(sum == target)
+            {
+                return {left+1, right+1};
+            }
+            else if(sum < target)
+            {
+                left++;
+            }
+            else if(sum > target)
+            {
+                right--;
+            }
+        }
+        return {0,0};
+    }
     int binSearch(vector<int> &nums,int left,int right,int target)
     {
         while(left <= right)
@@ -16,6 +38,8 @@ public:
     }
     vector<int> twoSum(vector<int>& input, int target) 
     {
+        return solve(input, target);
+        
         int len = input.size();
         for(int i = 0; i < input.size(); i++)
         {
