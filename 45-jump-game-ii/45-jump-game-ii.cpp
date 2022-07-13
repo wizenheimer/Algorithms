51f6cf9f4a841a961d33cb4b3530c0f06e483f3f
nayaN <91504165+wizenheimer@users.noreply.github.com>
2022-07-14 05:04:57 +0530

    Time: 15 ms (92.84%), Space: 16.6 MB (52.98%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) 
+    {
+        if(nums.size() <= 1)
+            return 0;
+        // keep the count of the largest ladder we have
+        int ladder = nums[0];
+        // keep the count of number of stairs on each ladder
+        int stairs = nums[0];
+        int result = 1;
+        for(int i = 1; i < nums.size(); i++)
+        {
+            // if we reach the end don't need to move on with the stairs
+            if(i == nums.size() - 1)
+                return result;
+            //  if we find a bigger ladder keep it until we run out of stairs      
+            if(i + nums[i] > ladder)
+            {
+                ladder = i + nums[i];
+            }
+            stairs--;
+            if(stairs == 0)
+            {
+                result++;
+                stairs = ladder - i;
+            }
+        }
+        return result;
+    }
+};
