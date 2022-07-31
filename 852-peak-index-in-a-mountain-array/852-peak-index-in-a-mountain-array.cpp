b639224ac9bc6f24ac2dfc06fedea70b5603caa5
nayaN <91504165+wizenheimer@users.noreply.github.com>
2022-07-31 21:17:03 +0530

    Time: 218 ms (9.59%), Space: 59.7 MB (28.87%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -3,17 +3,12 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int low = 0;
         int high = arr.size() - 1;
-        while(low <= high)
-        {
+        while(low <= high) {
             int mid = low + (high - low)/2;
-//          it's on the left rise
-            if(arr[mid] < arr[mid+1])
-            {
+            if(arr[mid] < arr[mid+1]) {
                 low = mid + 1;
             }
-//          it's on the right rise
-            else
-            {
+            else {
                 high = mid - 1;
             }
         }
