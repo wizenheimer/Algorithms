6476192b5c1022e2b3f42ff878b41490b2755929
nayaN <91504165+wizenheimer@users.noreply.github.com>
2022-10-20 21:29:10 +0530

    Time: 51 ms (56.67%), Space: 22.3 MB (55.67%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,30 @@
-class Solution {
-public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int result = nums[0];
-        for(int i = 1;  i < nums.size(); i++) {
-            result ^= nums[i];          
+class Solution
+{
+    public:
+        int singleNonDuplicate(vector<int> &nums)
+        {
+            if(nums.size() == 1) return nums[0];
+            
+            int left = 0, right = nums.size() - 1, res = -1;
+
+            while (left <= right)
+            {
+               	// identifying index
+                int mid = left + (right - left) / 2;
+                int index = (mid % 2 == 0) ? mid + 1 : mid - 1;
+
+               	// evaluation
+                if (nums[mid] == nums[index])
+                {
+                    // pattern doesn't break until mid
+                    left = mid + 1;
+                }
+                else
+                {
+                    // pattern has broken close the upper bound
+                    right = mid - 1;
+                }
+            }
+            return nums[left];
         }
-        return result;
-    }
 };
