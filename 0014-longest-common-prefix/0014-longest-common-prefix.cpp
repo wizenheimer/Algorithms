48edff40feb25b1e5864b1c8df7817986db6453f
nayaN <91504165+wizenheimer@users.noreply.github.com>
2022-10-29 03:30:55 +0530

    Time: 14 ms (20.41%), Space: 9.2 MB (53.86%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,24 +3,42 @@ class Solution
     public:
         string longestCommonPrefix(vector<string> &strs)
         {
-            std::sort(strs.begin(), strs.end(), []
-                (const std::string &first, const std::string &second)
-                {
-                    return first.size() < second.size();
-	});
+           	// runtime 20ms 
+           	//             std::sort(strs.begin(), strs.end(), []
+           	//                 (const std::string &first, const std::string &second)
+           	//                 {
+           	//                     return first.size() < second.size();
+           	// });
 
-            string prime = strs[0];
+           	//             string prime = strs[0];
+           	//             string result = "";
+
+           	//             for (int i = 0; i < prime.size(); i++)
+           	//             {
+           	//                 char ch = prime[i];
+           	//                 for (string str: strs)
+           	//                 {
+           	//                     if (ch != str[i]) return result;
+           	//                 }
+           	//                 result += ch;
+           	//             }
+           	//             return result;
+
+            std::sort(strs.begin(), strs.end());
+
+            string foo = strs[0];
+            string bar = strs[strs.size() - 1];
+
             string result = "";
 
-            for (int i = 0; i < prime.size(); i++)
+            for (int i = 0; i < foo.size(); i++)
             {
-                char ch = prime[i];
-                for (string str: strs)
-                {
-                    if (ch != str[i]) return result;
-                }
-                result += ch;
+                if (foo[i] == bar[i]) 
+                    result += foo[i];
+                else 
+                    return result;
             }
+            
             return result;
         }
 };
