f7aa001f87ddb3e013229dbf1dd2c2cc10a25ad1
nayaN <91504165+wizenheimer@users.noreply.github.com>
2022-10-29 02:20:47 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (79.22%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -3,26 +3,43 @@ class Solution
     public:
         string removeOuterParentheses(string str)
         {
-            stack<char> para;
-            string build = "", result = "";
-            for (char ch: str)
+           	// using stack ~ 9ms
+           	//             stack<char> para;
+           	//             string build = "", result = "";
+           	//             for (char ch: str)
+           	//             {
+           	//                	// build string
+           	//                 build.push_back(ch);
+
+           	//                	// handle the stack
+           	//                 if (ch == '(')
+           	//                     para.push('(');
+           	//                 else
+           	//                     para.pop();
+
+           	//                	// release the build
+           	//                 if (para.empty())
+           	//                 {
+           	//                     if(build.size() > 2)
+           	//                         result += build.substr(1, build.size() - 2);
+           	//                     build = "";
+           	//                 }   
+           	//             }
+
+           	// without using stacks ~ 0ms
+            int count = 0;
+            string result = "";
+
+            for (auto ch: str)
             {
-               	// build string
-                build.push_back(ch);
-
-               	// handle the stack
                 if (ch == '(')
-                    para.push('(');
-                else
-                    para.pop();
-
-               	// release the build
-                if (para.empty())
                 {
-                    if(build.size() > 2)
-                        result += build.substr(1, build.size() - 2);
-                    build = "";
-                }   
+                    if (count++) result.push_back(ch);
+                }
+                else
+                {
+                    if (--count) result.push_back(ch);
+                }
             }
             return result;
         }
