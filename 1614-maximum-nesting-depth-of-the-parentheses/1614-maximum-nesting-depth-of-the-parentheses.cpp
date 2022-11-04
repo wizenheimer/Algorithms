17bbec6d16c99b787ecffca813003e9915239026
nayaN <91504165+wizenheimer@users.noreply.github.com>
2022-11-04 12:01:07 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (56.03%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        stack<char> stk;
-        int result = 0;
+        // stack<char> stk;
+        // int result = 0;
+        // for(char ch : s) {
+        //     if(ch == '(') {
+        //         stk.push(ch);
+        //         result = result > stk.size() ? result : stk.size();
+        //     } else if(ch == ')') {
+        //         stk.pop();
+        //     }
+        // }
+        int result = 0, count = 0;
         for(char ch : s) {
-            if(ch == '(') {
-                stk.push(ch);
-                result = result > stk.size() ? result : stk.size();
-            } else if(ch == ')') {
-                stk.pop();
+            switch(ch) {
+                case '(':
+                    result = result > ++count ? result : count;
+                    break;
+                case ')':
+                    count--;
+                    break;
             }
         }
         return result;
