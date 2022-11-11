9a257f16876f228742cc66dfb1044ede8c4037ea
nayaN <91504165+wizenheimer@users.noreply.github.com>
2022-11-11 15:04:44 +0530

    Time: 212 ms (98.67%), Space: 110.4 MB (96.57%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,48 +1,26 @@
 /**
  *Definition for singly-linked list.
  *struct ListNode {
- *int val;
- *ListNode * next;
- *ListNode() : val(0), next(nullptr) {}
- *ListNode(int x) : val(x), next(nullptr) {}
- *ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
  *};
  */
 class Solution
 {
     public:
-        ListNode* reverse(ListNode *head)
+        bool isPalindrome(ListNode *head)
         {
-            ListNode *prev = NULL;
-            ListNode *next = NULL;
-            while (head != NULL)
-            {
-                next = head->next;
-                head->next = prev;
-                prev = head;
-                head = next;
-            }
-            return prev;
+            ListNode *slow = head, *fast = head, *prev = NULL, *tmp;
+            while (fast && fast->next)
+                fast = fast->next->next,
+                tmp = slow->next, slow->next = prev, prev = slow, slow = tmp;
+            slow = (fast ? slow->next : slow);
+            while (slow != NULL)
+                if (slow->val != prev->val) return false;
+                else slow = slow->next, prev = prev->next;
+            return true;
         }
-    bool isPalindrome(ListNode *head)
-    {
-        ListNode *slow = head;
-        ListNode *fast = head;
-
-        while (fast->next != NULL && fast->next->next != NULL)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        slow->next = reverse(slow->next);
-        slow = slow->next;
-        ListNode *dummy = head;
-        while (slow != NULL)
-        {
-            if (slow->val != dummy->val) return false;
-            slow = slow->next;
-            dummy = dummy->next;
-        }
-        return true;
-    }
 };
