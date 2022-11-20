07a83a408db5fe9739a9725707cdc29803133ee9
Nayan <91504165+wizenheimer@users.noreply.github.com>
2022-11-20 13:06:12 +0530

    Time: 3 ms (98.65%), Space: 9.1 MB (13.88%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+    public:
+        vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+        {
+            stack<int> Stack;
+            map<int, int> hash;
+            for (int i = nums2.size() - 1; i >= 0; i--)
+            {
+                int num = nums2[i];
+                
+                while (!Stack.empty())
+                {
+                    if (Stack.top() > num)
+                    {
+                        hash[num] = Stack.top();
+                        Stack.push(num);
+                        break;
+                    }
+                    else
+                    {
+                        Stack.pop();
+                    }
+                }
+
+                if (Stack.empty())
+                {
+                    hash[num] = -1;
+                    Stack.push(num);
+                }
+            }
+            vector<int> result(nums1.size(), -1);
+            for (int i = 0; i < nums1.size(); i++)
+            {
+                result[i] = hash[nums1[i]];
+            }
+            return result;
+        }
+};
