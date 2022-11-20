0a8d6472a9d569e029817a85886bfa5320e72730
Nayan <91504165+wizenheimer@users.noreply.github.com>
2022-11-20 19:30:10 +0530

    Time: 291 ms (73.42%), Space: 82.1 MB (37.52%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -3,15 +3,15 @@ private:
     int SIZE;
     vector<int> createNSL(vector<int> &heights) {        
         stack<pair<int, int>> Stack;
-        vector<int> result(SIZE, 0);
+        vector<int> result(SIZE, -1);
         
         for(int i = 0; i < SIZE; i++) {
             while(!Stack.empty() && Stack.top().first >= heights[i])
                 Stack.pop();
             if(!Stack.empty())
                 result[i] = Stack.top().second;
-            else
-                result[i] = -1;
+            // else
+            //     result[i] = -1;
             Stack.push({heights[i], i});
         }
         
@@ -20,15 +20,15 @@ private:
     vector<int> createNSR(vector<int> &heights) {
         // int SIZE = heights.size();
         stack<pair<int, int>> Stack;
-        vector<int> result(SIZE, 0);
+        vector<int> result(SIZE, SIZE);
         
         for(int i = SIZE - 1; i >= 0; i--) {
             while(!Stack.empty() && Stack.top().first >= heights[i])
                 Stack.pop();
             if(!Stack.empty())
                 result[i] = Stack.top().second;
-            else
-                result[i] = SIZE;
+            // else
+            //     result[i] = SIZE;
             Stack.push({heights[i], i});
         }
         
